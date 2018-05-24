d33c11c1e29a2afbbc1a9b6c237619d3c5f0ce2a
alepeculio <alejandropeculio@gmail.com>
2018-05-24 20:46:39 -0300

    Merge origin/master
    Conflicts:
    	web/vistas/adminHospitalMenu.jsp

--- src/Controladores/CEmpleado.java
+++ src/Controladores/CEmpleado.java
@@ -16,26 +16,7 @@ import java.util.Date;
 import java.util.List;
 
 public class CEmpleado {
-
-    /*
-    public static Turno obtenerTurno(long id) {
-        Turno turno = null;
-        try {
-            if (!Singleton.getInstance().getEntity().getTransaction().isActive()) {
-                Singleton.getInstance().getEntity().getTransaction().begin();
-            }
-            turno = (Turno) Singleton.getInstance().getEntity().createNativeQuery("SELECT * FROM turno WHERE id=" + id, Turno.class)
-                    .getSingleResult();
-            Singleton.getInstance().getEntity().getTransaction().commit();
-        } catch (Exception e) {
-            if (Singleton.getInstance().getEntity().getTransaction().isActive()) {
-                Singleton.getInstance().getEntity().getTransaction().rollback();
-            }
-            System.err.println("No se puedo encontrar el turno con id: " + id);
-        }
-        return turno;
-    }
-     */
+    
     public static HorarioAtencion obtenerHorarioAtencion(long id) {
         HorarioAtencion horarioAtencion = null;
         try {
